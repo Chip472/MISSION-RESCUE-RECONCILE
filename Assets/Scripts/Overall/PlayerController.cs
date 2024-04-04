39c648effe7415c0e3488af653e8b4c242222d50
Chip <nnthuylinh2006@gmail.com>
2024-04-04 10:39:14 +0700

    add gun effect

--- Assets/Scripts/Overall/PlayerController.cs
+++ Assets/Scripts/Overall/PlayerController.cs
@@ -8,8 +8,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float runSpeed = 40f;
 
     [SerializeField] private Animator playerAnim;
+    [SerializeField] private GameObject gunEffect;
 
     float horizontalMove = 0f;
+    bool gunEffectCheck = false;
 
     // Update is called once per frame
     void Update()
@@ -23,6 +25,29 @@ public class PlayerController : MonoBehaviour
         {
             playerAnim.SetBool("run", false);
         }
+
+        if (Input.GetKeyDown("space") || Input.GetMouseButtonDown(0))
+        {
+            if (!gunEffectCheck)
+            {
+                StartCoroutine(DelayGunEffect());
+            }
+        }
+    }
+
+    IEnumerator DelayGunEffect()
+    {
+        gunEffectCheck = true;
+        playerAnim.SetBool("run", true);
+        playerAnim.Play("BlitzRun", 0);
+        gunEffect.SetActive(true);
+
+        yield return new WaitForSeconds(0.2f);
+        gunEffect.SetActive(false);
+
+        yield return new WaitForSeconds(0.1f);
+        playerAnim.SetBool("run", false);
+        gunEffectCheck = false;
     }
 
     private void FixedUpdate()
